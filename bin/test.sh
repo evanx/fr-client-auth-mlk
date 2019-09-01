7646f3bfada87d07db7d934d4ef55d280c67cbe8
Evan Summers <evan.summers@strongbyte.com>
2019-09-01 13:21:58 +0200

    Introduce register and login endpoints

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-if [ $NODE_ENV != 'development' ]
+if [ $NODE_ENV = 'production' ]
 then
   echo "Unsupported NODE_ENV: $NODE_ENV"
   exit 1
@@ -8,8 +8,17 @@ fi
 
 set -x 
 
+curl -X 'POST' -d 'client=test&secret=test' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H "Accept: application/json" \
+  http://127.0.0.1:3000/register
+
+
+exit 0 
+
 curl -X 'POST' -d 'type=test&source=test' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -H "Accept: application/json" \
+  -H "Authorization: Bearer abc123" \
   http://127.0.0.1:3000/xadd/mystream:x 
 
